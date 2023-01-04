935d8b9832294a0d02c4cdaee42f87ea65b2726f
hansey <vanhanseyll@gmail.com>
2023-01-04 14:08:23 -0500

    Connection MongoDB and creating users working

--- CRUDNoSQLMongoDB/src/main/java/com/mycompany/crudnosqlmongodb/CRUDNoSQLMongoDB.java
+++ CRUDNoSQLMongoDB/src/main/java/com/mycompany/crudnosqlmongodb/CRUDNoSQLMongoDB.java
@@ -1,8 +1,12 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.crudnosqlmongodb;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 
 /**
  *
@@ -11,6 +15,36 @@ package com.mycompany.crudnosqlmongodb;
 public class CRUDNoSQLMongoDB {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MongoClient mongo = createConnection();
+
+        //if doesnt exists the database we create it
+        if (mongo != null) {
+            DB db = mongo.getDB("Test");
+            System.out.println("database created");
+            //creating collection if it doesnt exists
+            //inserting doc to the collection
+            insertUser(db,"users","Hansey","Colombia");
+            insertUser(db,"users","Hansey2","Colombia2");
+            insertUser(db,"users","Hansey3","Colombia3");
+
+        }
+    }
+
+    public static MongoClient createConnection() {
+        System.out.println("test");
+        MongoClient mongo = null;
+        mongo = new MongoClient("localhost", 27017);
+        return mongo;
+    }
+    
+    public static void insertUser(DB db, String collection,String name,String country){
+    
+        DBCollection colect = db.getCollection(collection);
+        //Create doc and insert received info
+        BasicDBObject document = new BasicDBObject();
+        document.put("name",name);
+         document.put("country",country);
+         colect.insert(document);
+         
     }
 }
